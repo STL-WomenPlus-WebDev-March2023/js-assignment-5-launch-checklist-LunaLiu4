9ecf46d227a8641bc1f46beacdee66191a008173
LunaLiu4 <luna.liu829@gmail.com>
2023-07-07 22:38:35 -0500

    modified fetch

--- script.js
+++ script.js
@@ -2,20 +2,6 @@
 
 window.addEventListener("load", function () {
 
-    let listedPlanets;
-    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-    let listedPlanetsResponse = myFetch();
-    listedPlanetsResponse.then(function (result) {
-        listedPlanets = result;
-        console.log(listedPlanets);
-    }).then(function () {
-        console.log(listedPlanets);
-        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-        let selectedPlanet = pickPlanet(listedPlanets);
-        console.log(selectedPlanet);
-        addDestinationInfo(document, selectedPlanet.name, selectedPlanet.diameter, selectedPlanet.star, selectedPlanet.distance, selectedPlanet.moons, selectedPlanet.image);
-    })
-
     let form = document.querySelector('form');
     let list = document.getElementById('faultyItems');
     list.style.visibility = 'hidden';
@@ -29,6 +15,18 @@ window.addEventListener("load", function () {
         // console.log(typeof (fuelLevel));
         // console.log(pilot, copilot, fuelLevel, cargoLevel);
         formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel);
-
+        let listedPlanets;
+        // Set listedPlanetsResponse equal to the value returned by calling myFetch()
+        let listedPlanetsResponse = myFetch();
+        listedPlanetsResponse.then(function (result) {
+            listedPlanets = result;
+            console.log(listedPlanets);
+        // }).then(function () {
+            // console.log(listedPlanets);
+            // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+            let selectedPlanet = pickPlanet(listedPlanets);
+            console.log(selectedPlanet);
+            addDestinationInfo(document, selectedPlanet.name, selectedPlanet.diameter, selectedPlanet.star, selectedPlanet.distance, selectedPlanet.moons, selectedPlanet.image);
+        })
     })
 });
